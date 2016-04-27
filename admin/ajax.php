0f6e8d47139854dcfc0d87c4cc5b348a3327fa92
stud0 <stud0@uitclassroom.com>
2016-04-27 11:03:45 +0200

    remove ajax.php

--- admin/ajax.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-echo '/admin/index.html';
-//echo var_export($_GET).var_export($_POST).'<h1>'.'</h1>';
